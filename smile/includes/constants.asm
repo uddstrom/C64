a9e02e1ef80f6f90336188ed339ccb9e173e78db
Jesper Uddström <jesper@uddstrom.se>
2026-01-31 11:37:16 +0100

    add input constants

--- smile/includes/constants.asm
+++ smile/includes/constants.asm
@@ -1,6 +1,7 @@
 
 // -------------- CONSTANTS --------------
 
+#import "/constants/inputConstants.asm"
 #import "/constants/screenConstants.asm"
 #import "/constants/systemConstants.asm"
 #import "/constants/zeroPageConstants.asm"
--- smile/libraries/inputLibrary.asm
+++ smile/libraries/inputLibrary.asm
@@ -1,13 +1,5 @@
 
-.label JOYSTICK_2 = $dc00
-
-.label UP       = %00000001
-.label DOWN     = %00000010
-.label LEFT     = %00000100
-.label RIGHT    = %00001000
-.label FIRE     = %00010000
-
-.label JOYSTICK_2_IDLE = %01111111
+// -------------- INPUT LIBRARY --------------
 
 INPUT:
 {
