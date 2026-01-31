903a571b4f4058e74c0b4fc39172837aba1a95db
Jesper Uddström <jesper@uddstrom.se>
2026-01-31 11:48:03 +0100

    add map macros

--- smile/includes/gameCode.asm
+++ smile/includes/gameCode.asm
@@ -9,50 +9,13 @@ main:
     jsr SYSTEM.setup
 
 gameSetup:
-    // lda #1
-    // sta $0400
- 
-    // DrawChar(19,3,5,YELLOW) // S
-    // DrawChar(13,3,6,YELLOW) // M
-    // DrawChar(9,3,7,YELLOW)  // I
-    // DrawChar(12,3,8,YELLOW) // L
-    // DrawChar(5,3,9,YELLOW)  // E
-    //
-    // // ROW 0
-    // DrawTile(2,0,0)
-    // DrawTile(2,0,1)
-    // DrawTile(2,0,2)
-    // DrawTile(2,0,3)
-    // DrawTile(2,0,4)
-    // DrawTile(2,0,5)
-    // DrawTile(2,0,6)
-    // DrawTile(2,0,7)
-    //
-    // // ROW 1
-    // DrawTile(2,1,0)
-    // // ROW 2
-    // DrawTile(2,2,0)
-    //
-    // // ROW 11
-    // DrawTile(2,11,0)
-    // DrawTile(2,11,1)
-    // DrawTile(2,11,2)
-    // DrawTile(2,11,3)
-    // DrawTile(2,11,4)
-    // DrawTile(2,11,5)
-    // DrawTile(2,11,6)
-    // DrawTile(2,11,7)
-
-
-    lda #0
-    sta mapNr
-    jsr MAP.load
-
+    LoadMap(0)
     #import "/includes/sprite.asm"
 
 gameLoop:
     jsr delay
     jsr INPUT.readJoystick_2
+    // jsr SPRITE.update
     jmp gameLoop
 
 delay:
