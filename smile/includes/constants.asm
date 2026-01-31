687d8da5349705b216d7e2e558736e22523cb69f
Jesper Uddström <jesper@uddstrom.se>
2026-01-31 12:40:40 +0100

    add sprite constants and tables

--- smile/includes/constants.asm
+++ smile/includes/constants.asm
@@ -3,5 +3,6 @@
 
 #import "/constants/inputConstants.asm"
 #import "/constants/screenConstants.asm"
+#import "/constants/spriteConstants.asm"
 #import "/constants/systemConstants.asm"
 #import "/constants/zeroPageConstants.asm"
--- smile/includes/gameCode.asm
+++ smile/includes/gameCode.asm
@@ -10,7 +10,38 @@ main:
 
 gameSetup:
     LoadMap(0)
-    #import "/includes/sprite.asm"
+    // #import "/includes/sprite.asm"
+
+    // --- init player (sprite 0) ---
+    ldx #0 // sprite 0
+
+    lda #100
+    sta Sprite_X_Pos,x
+    sta Sprite_Y_Pos,x
+
+    // Should be done in raster interupt routine
+    sta $d000           // sprite 0 x coord
+    sta $d001           // sprite 0 y coord
+
+    // For later
+    // lda #10
+    // sta Sprite_V_Speed,x
+    // sta Sprite_H_Speed,x
+
+    lda #%00000001          // Enable bit for sprite 0
+    sta SPRITE_MULTICOLOR   // Enable multicolor for sprite 0
+    lda #%00000001          // Enable bit for sprite 0
+    sta SPRITE_ENABLE       // Enable sprite 0
+
+    lda #YELLOW
+    sta SPRITE_COLOR,x //$d027,0
+
+    lda #BLACK
+    sta SPRITE_EXTRA_COLOR_1
+
+    lda #WHITE
+    sta SPRITE_EXTRA_COLOR_2
+
 
 gameLoop:
     jsr delay
