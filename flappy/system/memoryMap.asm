b1177d949f14b0f136a5b48281f1adab1bee363a
Jesper Uddström <jesper@uddstrom.se>
2026-03-02 20:37:37 +0100

    rearrange memory a bit to fix graphics bug

--- flappy/system/memoryMap.asm
+++ flappy/system/memoryMap.asm
@@ -3,9 +3,8 @@
 
 .label ZERO_PAGE_ADDRESS            = $0020
 .label GAME_CODE_ADDRESS            = $0810
-.label VARIABLES_ADDRESS            = $0c00
-.label CHARSET_ATTRIB_ADDRESS       = $0d00
-.label TILESET_ADDRESS              = $0e00
+.label CHARSET_ATTRIB_ADDRESS       = $0f00
+.label VARIABLES_ADDRESS            = $1000
 
 .label SPRITES_ADDRESS              = $2000 // 96 sprites * 64 byte/sprite = 6144 bytes = $1800 bytes
 .label CHARSET_ADDRESS              = $3800
